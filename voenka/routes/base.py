413dae2087597887101ba3ad9cf3ffab228a4a2d
danil <danil.mirtalibov.27@mail.ru>
2021-02-11 00:34:12 +0300

    Templates added.

--- voenka/routes/base.py
+++ voenka/routes/base.py
@@ -7,3 +7,13 @@ base = Blueprint("base", __name__)
 @base.route("/")
 def hello():
     return render_template("homepage.html")
+
+
+@base.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@base.route("/money")
+def money():
+    return render_template("money.html")
