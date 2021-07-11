b30e47c48ab95f332da283582af166c80c7a4d7f
Dan <danil.iavonov.27@mail.ru>
2021-07-11 14:25:04 +0300

    fix

--- voenka/routes/qr_code.py
+++ voenka/routes/qr_code.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
+
 from .logic.mongo_client import MongoConnector
 
 
@@ -10,7 +11,7 @@ print(mongo_client)
 def me():
     return render_template("qr/qr_base.html")
 
-@qr.route("/")
+@qr.route("/", methods=["GET"])
 def main():
     return render_template("qr/qr_index.html")
 
@@ -19,6 +20,12 @@ def search(id):
     global mongo_client
 
     data = mongo_client.find_document(id)
-    print(data)
     if data:
         return render_template("qr/qr_base.html", FIO = data["fio"], birth_date = data["birth_date"], end_date = "12.10.2022")
+
+@qr.route('/', methods=['POST'])
+def form_worker():
+    birth_date = request.form.get("birth_date")
+    fio = request.form.get("fio")
+    mongo_id = str(mongo_client.add({"fio":fio, "birth_date":birth_date}))
+    return search(mongo_id)
