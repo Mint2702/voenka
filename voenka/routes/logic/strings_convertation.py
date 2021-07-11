658f26452cda7a7108406995ce4841fc318a10fa
Dan <danil.iavonov.27@mail.ru>
2021-07-11 15:18:07 +0300

    fix

--- voenka/routes/logic/strings_convertation.py
+++ voenka/routes/logic/strings_convertation.py
@@ -1,8 +1,13 @@
 def fio_convert(fio: str) -> str:
+    fio = fio.strip()
     fio = fio.split(" ")
     new_fio = ""
-    for name in fio:
+    for i in range(3):
+        name = fio[i]
         word = name[0]
         word += "*" * (len(name)-1)
         new_fio += f"{word} "
     return new_fio
+
+test = fio_convert("Петров Глеб Васильевич   ")
+print(test)
